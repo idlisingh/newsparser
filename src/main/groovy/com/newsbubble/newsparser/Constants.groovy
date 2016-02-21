f1f3a70e7a9c441bb818f301bf720c788e831b9d
idlisingh <idlisingh@outlook.com>
2016-02-21 01:16:02 -0500

    Adding Fiorina

--- src/main/groovy/com/newsbubble/newsparser/Constants.groovy
+++ src/main/groovy/com/newsbubble/newsparser/Constants.groovy
@@ -2,5 +2,5 @@ package com.newsbubble.newsparser
 
 class Constants {
 
-    def static List<String> CANDIDATES = [ "sanders", "clinton", "trump", "carson", "cruz", "rubio", "kasich", "bush" ]
+    def static List<String> CANDIDATES = [ "sanders", "clinton", "trump", "carson", "cruz", "rubio", "kasich", "bush", "fiorina" ]
 }
